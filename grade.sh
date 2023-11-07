b48d06fb024213505c98507734b6b5fe3745e736
ziyexiaohei <yul303@ucsd.edu>
2023-11-07 11:55:20 -0800

    sdfas
    sdfsdfas

--- grade.sh
+++ grade.sh
@@ -8,6 +8,27 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if  [ -f "student-submission\ListExamples.java" ]; then
+    echo "You submitted the correct file"
+    else
+    echo "You submitted the wrong File, please check"
+fi
+
+cp -r student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
+
+if  [[ ! "$?" = 0]] then
+    echo "There is some error occur,please check you code"
+else 
+    echo "Your code is correct"
+fi
+
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
